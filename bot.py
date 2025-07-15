66959860675f1c6a7b6f8ed4c9f6c085df076715
Mr Kevin <ganeshsharma2346@gmail.com>
2025-07-16 01:15:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,11 +42,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeflixSupport for support")
+            self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Im_Mr_Kevin for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeflixSupport")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Im_Mr_Kevin")
         self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @CODEFLIX_BOTS""")
 
         self.set_parse_mode(ParseMode.HTML)
